5a07808a1a046820137f0e1f014afd1019072ab9
Vladimir <vlad.jarmola@gmail.com>
2023-07-13 11:52:49 +0600

    fix

--- trash.js
+++
@@ -1,7 +0,0 @@
-// require('dotenv').config();
-// console.log(process.env.REACT_APP_API_OPENWEATHERMAP)
-
-const dotenv = require('dotenv')
-const buf = Buffer.from('BASIC=basic')
-const config = dotenv.parse(buf) // will return an object
-console.log(typeof config, config) // o
